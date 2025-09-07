72903e347bedd05444cd8e869aaff253ec3f23cb
Jatin Vishwakarma <vishwakarmajatin084@gmail.com>
2025-09-07 12:33:07 +0530

    initial commit

---
+++ 02 Conditional Statements/if_elif_else.py
@@ -0,0 +1,9 @@
+money = int(input("Mom please provide me the money: "))
+
+if money == 10:
+      print("I  will have a choco bar icecream")
+
+elif money == 20:
+      print("I will have a mongo dolly")
+else:
+      print("I will have a cone icecream")
