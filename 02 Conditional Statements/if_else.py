7d0e39fafedc07cf5c4ee60c8731b454638b5777
Jatin Vishwakarma <vishwakarmajatin084@gmail.com>
2025-09-07 12:32:37 +0530

    initial commit

---
+++ 02 Conditional Statements/if_else.py
@@ -0,0 +1,15 @@
+a = 13
+
+if a > 10:
+      print("I will do task A")
+else:
+      print("I will do task B")
+
+
+money = int(input("Mom please provide me the money: "))
+
+if money == 10:
+      print("I  will have a choco bar icecream")
+
+else:
+      print("I will have a mongo dolly")
