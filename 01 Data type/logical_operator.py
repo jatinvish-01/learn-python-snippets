ffdaa021f27b64dd0c84c2e89147e57b80eb67a4
Jatin Vishwakarma <vishwakarmajatin084@gmail.com>
2025-09-07 12:31:44 +0530

    initial commit

---
+++ 01 Data type/logical_operator.py
@@ -0,0 +1,11 @@
+# AND 
+print(123 > 100 and 34 == 34) #True
+print(123 > 100 and 34 == 34 and 12 > 20) #False
+
+# OR
+print(12 != 12 or 34 == 64 or 54 == 90 or 21 > 12) #True
+
+# NOT
+print(not 12 == 12)
+
+print(True and bool(0)) #False
