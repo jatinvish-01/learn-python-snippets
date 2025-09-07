9b16f262afbffb57d6ffc67a24aa5c24660a9991
Jatin Vishwakarma <vishwakarmajatin084@gmail.com>
2025-09-07 20:04:57 +0530

    Initial commit

---
+++ 03 Loops/break_continue.py
@@ -0,0 +1,12 @@
+# Break
+for i in range(1, 21):
+      if i == 15:
+            break
+      print(i)
+
+
+# Continue
+for i in range(1, 21):
+      if i == 15:
+            continue # skip 15
+      print(i)
