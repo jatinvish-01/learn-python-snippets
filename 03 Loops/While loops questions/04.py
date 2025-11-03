c4cdeb368aefd2d93a8f483de12665b2fab5aa3f
Jatin <vishwakarmajatin084@gmail.com>
2025-11-03 11:07:12 +0530

    Initial commit

--- 03 Loops/While loops questions/04.py
+++ 03 Loops/While loops questions/04.py
@@ -1,1 +1,27 @@
-# Create a random number guessing game with python.
+# Create a random number      
+
+import random 
+
+num = random.randint(1, 21)
+
+tries = 0
+
+while True:
+      guess = int(input("Enter your guess btw 1 - 20: "))
+
+      if num == guess:
+            tries +=1
+            print(f"You Won.. you guessed the number {tries} tries")
+            break
+
+      elif num < guess:
+            print("Go a litle lower")
+            tries +=1
+
+      elif num > guess:
+            print("Go a litle higher")
+            tries +=1
+
+      else:
+            tries +=1
+            print("You Loos..!!") 
